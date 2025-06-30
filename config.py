6a33debeea33301408f1e539e102a6160de907e7
Hiller Liao <hillerliao@163.com>
2025-06-30 16:22:38 +0800

    update message sending function to support n8n channel and adjust memo retrieval logic

--- config.py
+++ config.py
@@ -13,3 +13,9 @@ TG_BOT_TOKEN = "Telegram Bot API Token"
 # Telegram Chat ID
 CHAT_ID = "chat id"
 
+# n8n webhook URL for message forwarding
+N8N_WEBHOOK_URL = "https://n8n.domain.com/webhook/MsgForward"
+
+# n8n webhook token for authentication
+N8N_WEBHOOK_TOKEN = "__n8n_BLANK_VALUE_e5362baf-c777-4d57-a609-and_else"
+
--- random_memo.py
+++ random_memo.py
@@ -21,7 +21,8 @@ def get_random_memo(db='', filter=FILTER):
         row = c.fetchone()
 
         # exclude memo cotaining certain characters.
-        if filter not in row[5]:
+        #if filter not in row[5]:
+        if 'https://' in row[5]:
             msg = row[5]
 
             if len(msg) <= 500:
@@ -29,23 +30,34 @@ def get_random_memo(db='', filter=FILTER):
             else:
                 msg = msg[0:500] + '...'
 
-            link = f'https://{DOMAIN}/m/{row[0]}'
+            link = f'https://{DOMAIN}/m/{row[7]}'
             msg = msg  + ' ' + link
             break
     conn.close()
     return msg
 
 
-def push_msg():
+def push_msg(channel='telegram'):
+    channel= channel.lower()
     msg = get_random_memo(db, filter=FILTER)
-    url = f'https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage'
-    params = {'chat_id': CHAT_ID, 'text': msg}
-    response = requests.post(url, params=params)
-
+    if channel== 'telegram':
+        if not TG_BOT_TOKEN or not CHAT_ID:
+            print('TG_BOT_TOKEN or CHAT_ID is not set.')
+            return
+        url = f'https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage'
+        params = {'chat_id': CHAT_ID, 'text': msg}
+        response = requests.post(url, params=params)
+    elif channel == 'n8n':
+        N8N_WEBHOOK_URL = N8N_WEBHOOK_URL.strip()
+        if not N8N_WEBHOOK_URL:
+            print('N8N_WEBHOOK_URL is not set.')
+            return
+        headers = {'token': N8N_WEBHOOK_TOKEN, 'Content-Type': 'application/json'}
+        response = requests.post(N8N_WEBHOOK_URL, json={'content': msg}, headers=headers)
     if response.status_code == 200:
         print('Message sent.')
     else:
         print('Message sending failed.')
 
 if __name__ == '__main__':
-    push_msg()
+    push_msg(channel='n8n')
